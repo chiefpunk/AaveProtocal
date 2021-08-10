533df24275c081830e537e03334ee58ff1f5d9bd
WebDevStar <webdevstar14@gmail.com>
2021-08-10 06:09:59 -0600

    Update aDAI address

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,4 @@ export const LendingPoolContractAddress =
   '0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe'
 
 export const DAITokenAddress = '0xff795577d9ac8bd7d90ee22b6c1703490b6512fd'
-export const aDAITokenAddress = '0xd0c6883f5fa3e042509860f25ff614d139e78c4b'
+export const aDAITokenAddress = '0xdcf0af9e59c002fa3aa091a46196b37530fd48a8'
